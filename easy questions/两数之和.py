ad5acf0e4ebc883490996af396f4b440dbbce5c2
kit-leonzhang <52654921+kit-leonzhang@users.noreply.github.com>
2020-10-24 18:54:10 +0200

    add files of “两数之和”
    add files actions

---
+++ easy questions/两数之和.py
@@ -0,0 +1,24 @@
+# def twosum(nums, target):
+#    n = len(nums)
+#    for i in range(n):
+#        for j in range(i + 1, n):
+#            if nums[i] + nums[j] == target:
+#                return [i, j]
+
+
+def twosum(nums, target):
+    lens = len(nums)
+    j = -1
+    for i in range(1, lens):
+        temp = nums[:i]
+        if (target - nums[i]) in temp:
+            j = temp.index(target - nums[i])
+            break
+    if j >= 0:
+        return [j, i]
+
+
+if __name__ == "__main__":
+    N = [2, 7, 11, 15]
+    T = 9
+    print(twosum(N, T))
