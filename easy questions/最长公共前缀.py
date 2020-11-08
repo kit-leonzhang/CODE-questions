5e4fc25e04fd60510a8fc917adec580b9b5852d8
kit-leonzhang <52654921+kit-leonzhang@users.noreply.github.com>
2020-11-08 14:41:01 +0100

    [数组]14------二维数组，可以用zip法
    zip   set的用法

---
+++ easy questions/最长公共前缀.py
@@ -0,0 +1,14 @@
+def longestCommonPrefix(strs):
+    if len(strs) == 0:
+        return ''
+    for i in range(len(strs[0])):
+        c = strs[0][i]
+        for j in range(1, len(strs)):
+            if i == len(strs[j]) or strs[j][i] != c:
+                return strs[0][0:i]
+    return strs[0]
+
+
+if __name__ == "__main__":
+    N = ["flower", "flow", "flight"]
+    print(longestCommonPrefix(N))
